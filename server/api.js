7b9a964efdeff80bb727293f8716de501a1fceb0
William Simmons <willsimmons@gmail.com>
2016-11-11 15:59:00 -0800

    addition of museum endpoint, need to know client location lat/long

--- server/api.js
+++ server/api.js
@@ -5,10 +5,15 @@ const router = express.Router();
 
 //in the future we can do (/'endpoint', check Redis for result, googleAPIController)
 router.get('/museum', (req, res) => {
-  console.log('look for coordinates in here', req);
-  googleController.getData('museum', lat, long, (data) => {
-    console.log('google stuff', data);
-    res.status(200).json(data);
+  // console.log('look for coordinates in here', req);
+  googleController.getMuseumData(37, -122, (data) =>{
+    console.log('answer', data);
+    if (data === null) {
+      res.status(204).send('our response'); 
+    } else {
+      console.log('our response');
+      res.status(200).json(data); 
+    }
   });
 });
 
--- server/googleController.js
+++ server/googleController.js
@@ -1,24 +1,24 @@
 const request = require('request');
 //google API url - may have to adjust the radius part of the query
-const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?&radius=500&key=${process.env.GOOGLEKEY}`;
+const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?&radius=500';
 
-const getData = (type, lat, long, cb) => {
+const getMuseumData = (lat, long, callback) => {
   //hard coding to be replaced once client sends lat and long
   //delete 10 and 11 once we are getting location data from the client
   lat = lat || 37.7825177;
   long = long || -122.4106772;
-
-  return request(`${url}&type=${type}&location${lat},${long}`, (err, data) => {
+  request(`${url}&type=museum&location=${lat},${long}&key=${process.env.GOOGLEKEY}`, (err, res, body) => {
     if (err) {
       console.log('no places found by google');
-      return cb(err, null);
+      callback(err, null);
     }
-    return cb(data);
+    console.log('google response', body);
+    callback(err, body);
   });
 };
 
 module.exports = {
-  getData
+  getMuseumData
 };
 //replace line 10's first parameter with 
 // `${url}&type=${typeOfLocation}&location=${currentLocation}`
