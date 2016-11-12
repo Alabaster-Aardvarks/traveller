220bb27c833ad6af4f8634f080c1324b922df27d
William Simmons <willsimmons@gmail.com>
2016-11-11 19:37:39 -0800

    we are getting the data from google, now we need to know what the client needs

--- server/api.js
+++ server/api.js
@@ -6,17 +6,26 @@ const router = express.Router();
 //in the future we can do (/'endpoint', check Redis for result, googleAPIController)
 router.get('/museum', (req, res) => {
   // console.log('look for coordinates in here', req);
-  googleController.getMuseumData(37.7825177, -122.4106772, (data) =>{
-    console.log('answer', data);
-    if (data === null) {
-      res.status(204).end(); 
+  googleController.getMuseumData(37.7825177, -122.4106772, (err, data) =>{
+    if (err) {
+      //parsing error
+      res.send(err);
     } else {
-      console.log('our response');
-      res.status(200).json(data); 
+      //sending back the client response
+      if (data === null) {
+        //data was not found
+        res.sendStatus(204); 
+      } else {
+        //data is found
+        res.status(200).json(data); 
+      }
     }
   });
 });
 
-
+//404 all other routes
+router.use('*', (req, res) => {
+  res.status(404).send();
+});
 
 module.exports = router;
--- server/googleController.js
+++ server/googleController.js
@@ -1,21 +1,21 @@
+//controller for all Google API requests
+
 const request = require('request');
-//google API url - may have to adjust the radius part of the query
+
 const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?&radius=500';
+
 const getMuseumData = (lat, long, callback) => {
-  //hard coding lat and long in api.js to be replaced once client sends lat and long
   request(`${url}&type=museum&location=${lat},${long}&key=${process.env.GOOGLEKEY}`, (err, res, body) => {
     if (err) {
-      console.log('no places found by google');
-      callback(err, null);
+      //no places found by google
+      return callback(err, null);
     }
-    console.log('google response', body);
-    callback(err, body);
+    //google has responded
+    const data = JSON.parse(body);
+    return callback(err, data.results);
   });
 };
 
 module.exports = {
   getMuseumData
 };
-//replace line 10's first parameter with 
-// `${url}&type=${typeOfLocation}&location=${currentLocation}`
-// replace #8 with callback
