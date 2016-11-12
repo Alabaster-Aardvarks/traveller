ff8632577b5459c5bf93645fa057972b97012555
William Simmons <willsimmons@gmail.com>
2016-11-11 16:17:16 -0800

    adjustments to url API call based on future parameters of client location data

--- server/googleController.js
+++ server/googleController.js
@@ -1,12 +1,8 @@
 const request = require('request');
 //google API url - may have to adjust the radius part of the query
 const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?&radius=500';
-
 const getMuseumData = (lat, long, callback) => {
-  //hard coding to be replaced once client sends lat and long
-  //delete 10 and 11 once we are getting location data from the client
-  lat = lat || 37.7825177;
-  long = long || -122.4106772;
+  //hard coding lat and long in api.js to be replaced once client sends lat and long
   request(`${url}&type=museum&location=${lat},${long}&key=${process.env.GOOGLEKEY}`, (err, res, body) => {
     if (err) {
       console.log('no places found by google');
