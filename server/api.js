2140eb64681caab5bc240297f568d1a8e7b23450
William Simmons <willsimmons@gmail.com>
2016-11-14 11:53:23 -0800

    rename base api router to placeRouter

--- server/api.js
+++ server/api.js
@@ -1,13 +1,13 @@
 // this is the router that instructs the server 'what to do' depending on which endpoint
 const express = require('express');
 const googleController = require('./googleController');
-const router = express.Router(); 
+const placeRouter = express.Router(); 
 
 //in the future we can do (/'endpoint', check Redis for result, googleAPIController)
 //refactor googleController's get functionality
 //refactor router callback!
 //
-router.get('/museum', (req, res) => {
+placeRouter.get('/museum', (req, res) => {
   // console.log('look for coordinates in here', req);
   googleController.getData('museum', 37.7825177, -122.4106772, (err, data) =>{
     if (err) {
@@ -26,7 +26,7 @@ router.get('/museum', (req, res) => {
   });
 });
 
-router.get('/park', (req, res) => {
+placeRouter.get('/park', (req, res) => {
   // console.log('look for coordinates in here', req);
   googleController.getData('park', 37.7825177, -122.4106772, (err, data) =>{
     if (err) {
@@ -45,7 +45,7 @@ router.get('/park', (req, res) => {
   });
 });
 
-router.get('/bank', (req, res) => {
+placeRouter.get('/bank', (req, res) => {
   // console.log('look for coordinates in here', req);
   googleController.getData('bank', 37.7825177, -122.4106772, (err, data) =>{
     if (err) {
@@ -68,8 +68,8 @@ router.get('/bank', (req, res) => {
 
 
 //404 all other routes
-router.use('*', (req, res) => {
+placeRouter.use('*', (req, res) => {
   res.status(404).send();
 });
 
-module.exports = router;
+module.exports = placeRouter;
