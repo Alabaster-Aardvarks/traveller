9ed97d6f6af32e74b75c17ae0ba7c1ec35cf2723
Will Simmons <willsimmons@gmail.com>
2016-11-15 13:45:04 -0800

    Keeping same as commit  b182fe0

--- traveller/App/Components/MapCallout.js
+++ traveller/App/Components/MapCallout.js
@@ -1,18 +1,28 @@
-// @flow
-
-import React, { PropTypes } from 'react'
+import React from 'react'
 import { Text, TouchableOpacity } from 'react-native'
 import MapView from 'react-native-maps'
 import Styles from './Styles/MapCalloutStyle'
+import ExamplesRegistry from '../Services/ExamplesRegistry'
+
+// Example
+ExamplesRegistry.add('Map Callout', () =>
+  <MapCallout
+    location={{
+      title: 'Callout Example'
+    }}
+    onPress={() => window.alert('That tickles!')}
+  />
+)
+
+type MapCalloutProps = {
+  location: Object,
+  onPress: () => void
+}
 
 export default class MapCallout extends React.Component {
-  props: {
-    location: Object,
-    onPress: () => void
-  }
-  onPress: () => void
+  props: MapCalloutProps
 
-  constructor (props) {
+  constructor (props: MapCalloutProps) {
     super(props)
     this.onPress = this.props.onPress.bind(this, this.props.location)
   }
