005af2b7db39d62e1467b9d449868698f8da2823
William Simmons <willsimmons@gmail.com>
2016-11-16 11:51:47 -0800

    refactoring for promises and axios

--- server/controller/placesController.js
+++ server/controller/placesController.js
@@ -1,22 +1,17 @@
 //controller for all Google API requests
 //api guide https://developers.google.com/maps/documentation/javascript/places#place_search_requests
 //keyword guide https://developers.google.com/places/supported_types
-
-const request = require('request');
+const axios = require('axios');
 //1609 meters = 1 mile
 const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?&radius=1609';
 
-const getData = (place, lat, long, callback) => {
-  request(`${url}&type=${place}&location=${lat},${long}&key=${process.env.GOOGLEKEY}`, (err, res, body) => {
-    if (err) {
-      //no places found by google
-      return callback(err, null);
-    }
-    //google has responded
-    console.log(body);
-    const data = JSON.parse(body);
-    return callback(err, data.results);
-  });
+const getData = (place, lat, long) => {
+  return axios({
+    method: 'get',
+    url: `${url}&type=${place}&location=${lat},${long}&key=${process.env.GOOGLEKEY}`
+  })
+  .then(response => response.data)
+  .catch(error => console.error(error));
 };
 
 module.exports = {
