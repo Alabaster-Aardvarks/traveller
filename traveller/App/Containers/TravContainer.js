941cfb3a3e95b8f48e64bebf2fbcf271b42765cb
Daniel Shneyder <archcorsair@gmail.com>
2016-11-17 17:15:52 -0800

    Add ability to load isochrones in UI view

--- traveller/App/Containers/TravContainer.js
+++ traveller/App/Containers/TravContainer.js
@@ -1,28 +1,52 @@
-// @flow
-
 import React from 'react'
 import { connect } from 'react-redux'
-import { ScrollView, View, StyleSheet, Text, Dimensions, Slider } from 'react-native'
+import { ScrollView, View, StyleSheet, Text, Dimensions, Slider, StatusBar } from 'react-native'
 import { Actions as NavigationActions } from 'react-native-router-flux'
-import AlertMessage from '../Components/AlertMessage'
 import MapView from 'react-native-maps'
+import AlertMessage from '../Components/AlertMessage'
+import Spinner from 'react-native-spinkit'
 import { calculateRegion } from '../Lib/MapHelpers'
 import MapCallout from '../Components/MapCallout'
 import ListviewGridExample from './ListviewGridExample'
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
-// Default values
-const { width, height } = Dimensions.get('window');
-const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-
+import { updateIsochrons, setUpdateIsochronsStateFn, savedPolygons, terminateIsochronWorker,
+         isochronFillColor, ISOCHRON_NOT_LOADED, ISOCHRON_LOADING, ISOCHRON_LOADED } from './isochron'
 // Styles
 import styles from './Styles/TravContainerStyle'
 
+const COORDINATE_PRECISION = 0.001 // degrees
+const DATETIME_PRECISION = 60 // seconds
+const roundCoordinate = coord => {
+  return ( Math.round( Math.abs(coord) / COORDINATE_PRECISION ) * COORDINATE_PRECISION ) * Math.sign(coord)
+}
+const roundDateTime = dateTime => {
+  let date = new Date(dateTime)
+  // getTime() gives us milliseconds
+  date.setTime( Math.round( date.getTime() / (DATETIME_PRECISION * 1000) ) * (1000 * DATETIME_PRECISION) )
+  return date.toISOString()
+}
+const DATETIME = roundDateTime('2016-11-09T18:49:27.000Z')
+const DURATIONS = [ 0, 600, 1200, 1800, 2400, 3000, 3600, 4200 ]
+const LATITUDE = roundCoordinate(37.7825177)
+const LONGITUDE = roundCoordinate(-122.4106772)
+const LATITUDE_DELTA = roundCoordinate(0.1)
+const DOWNSAMPLING_COORDINATES = 5 // keep 1 point out of every 5
+
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const mapProvider = MapView.PROVIDER_GOOGLE
+
+let skipIsochrons = false // set to true to disable loading isochrons [for debug]
+updateIsochrons({ params: {
+  latitude: LATITUDE,
+  longitude: LONGITUDE,
+  durations: DURATIONS,
+  dateTime: DATETIME,
+  downSamplingCoordinates: DOWNSAMPLING_COORDINATES,
+  skip: skipIsochrons,
+}})
 
 class TravContainer extends React.Component {
   constructor (props) {
@@ -38,10 +62,30 @@ class TravContainer extends React.Component {
       },
       locations: [],
       showUserLocation: false,
+      isochronDurations: DURATIONS,
+      polygonsState: ISOCHRON_NOT_LOADED,
+      dateTime: DATETIME,
+      downSamplingCoordinates: DOWNSAMPLING_COORDINATES,
+      networkActivityIndicatorVisible: false,
+      spinnerVisible: true,
     }
   }
 
   componentDidMount() {
+    setUpdateIsochronsStateFn(this.updatePolygonsState.bind(this))
+
+    this.setState({ networkActivityIndicatorVisible: true, spinnerVisible: true })
+    setTimeout(() => this.updatePolygons({
+      isochrons: {
+        latitude: this.state.region.latitude,
+        longitude: this.state.region.longitude,
+        durations: this.state.isochronDurations,
+        dateTime: this.state.dateTime,
+        downSamplingCoordinates: this.state.downSamplingCoordinates,
+        skip: skipIsochrons
+      }
+    }), 500)
+
     let context = this;
     this.renderMapMarkers = this.renderMapMarkers.bind(this)
     this.onRegionChange = this.onRegionChange.bind(this)
@@ -73,7 +117,28 @@ class TravContainer extends React.Component {
       }
     console.tron.log(context.state.locations.latitude);
     //   this.refs.map.animateToCoordinate(testRegion, 50) // TODO: Center map on GPS coords
-  }, 1000)
+    }, 1000)
+  }
+
+  componentWillUnmount () {
+    setUpdateIsochronsStateFn(null)
+    terminateIsochronWorker()
+  }
+
+  updatePolygons (params) {
+    updateIsochrons({ params: params.isochrons })
+  }
+
+  updatePolygonsState (state) {
+    this.setState({ polygonsState: state })
+    this.setState({ networkActivityIndicatorVisible: (state === ISOCHRON_LOADED) ? false : true })
+    let context = this
+    if (state === ISOCHRON_LOADED) {
+      // delay the removal of the spinner overlay to give time for the isochrons to appear
+      setTimeout(() => { context.setState({ spinnerVisible: false }) }, 150)
+    } else {
+      this.setState({ spinnerVisible: true })
+    }
   }
 
   calloutPress (location) {
@@ -89,21 +154,22 @@ class TravContainer extends React.Component {
   }
 
   onRegionChange (region) {
+    // Update region when map is dragged
     this.setState({ region });
   }
 
   render () {
-
+    // wait for all polygons to be loaded
+    const polygonsCount = (savedPolygons && this.state.polygonsState === ISOCHRON_LOADED) ? savedPolygons.length : 0
     const coordinates = [ { latitude: LATITUDE, longitude: LONGITUDE },
                           { latitude: LATITUDE + 0.015, longitude: LONGITUDE - 0.015 },
                           { latitude: LATITUDE - 0.015, longitude: LONGITUDE - 0.005 },
                         ];
-
     return (
       <View style={styles.container}>
         <MapView
           ref='map'
-          provider={MapView.PROVIDER_GOOGLE}
+          provider={mapProvider}
           // style={styles.map}
           width={width}
           height={height}
@@ -112,6 +178,22 @@ class TravContainer extends React.Component {
           showsUserLocation={this.state.showUserLocation}
           >
             {this.state.locations.map((location) => this.renderMapMarkers(location))}
+            {this.state.locations.map(location => this.renderMapMarkers(location))}
+            { polygonsCount === 0 ? undefined : savedPolygons.map((pArray, arrayIndex) => {
+                return (pArray.length === 0) ? undefined : pArray.map((p, index) => {
+                  return (
+                    <MapView.Polygon
+                      coordinates={ p.polygon }
+                      holes={ p.holes }
+                      fillColor={ isochronFillColor(arrayIndex, 0.15) }
+                      strokeWidth={ 1 }
+                      strokeColor={ 'rgba(85, 85, 85, 0.5)' }
+                      key={ arrayIndex * 1000 + index }
+                    />
+                  )
+                })
+              })
+            }
           </MapView>
 
           <Slider step={0.25} style={{ position: 'absolute', right: 200, left: -125, top: 250, bottom: 100, height: 50, transform: [{ rotate: '270deg' }] }} />
