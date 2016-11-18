43821ba4e535f75b47d8b652949165d35a7bb1dc
Daniel Shneyder <archcorsair@gmail.com>
2016-11-18 12:33:02 -0800

    Add scene items for hamburger menu

--- traveller/App/Navigation/NavigationRouter.js
+++ traveller/App/Navigation/NavigationRouter.js
@@ -20,6 +20,8 @@ class NavigationRouter extends Component {
         <Scene key='drawer' component={NavigationDrawer} open={false}>
           <Scene key='drawerChildrenWrapper' navigationBarStyle={Styles.navBar} titleStyle={Styles.title} leftButtonIconStyle={Styles.leftButton} rightButtonTextStyle={Styles.rightButton}>
             <Scene initial key='presentationScreen' component={TravContainer} title='Traveller' renderLeftButton={NavItems.hamburgerButton} />
+            <Scene key='travContainer' component={TravContainer} title='Traveller' />
+            <Scene key='deviceInfo' component={DeviceInfoScreen} title='Device Info' navBar={CustomNavBar} />
           </Scene>
         </Scene>
       </Router>
