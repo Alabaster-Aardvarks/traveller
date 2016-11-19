122ec44047d662fa82cd27193f3dd0574a85b7d7
William Simmons <willsimmons@gmail.com>
2016-11-18 17:44:33 -0800

    working on api calls, next phase is testing

--- server/controller/placesController.js
+++ server/controller/placesController.js
@@ -3,16 +3,32 @@
 //keyword guide https://developers.google.com/places/supported_types
 const axios = require('axios');
 //1609 meters = 1 mile
-const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?&radius=1609';
+//4827 meters = 3 miles
+
+//radar search # step 1
+const radar = "https://maps.googleapis.com/maps/api/place/radarsearch/json?"
+const distance = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric"
+
 const key = process.env.GOOGLE_KEY || GOOGLE_KEY;
 
-const getData = (place, lat, long) => {
-  lat = lat || 37.7825177;
-  long = long || -122.4106772;
+const getRadarData = (typeOfPlace, userLat, userLong) => {
+  userLat = userLat || 37.7825177;
+  userLong = userlong || -122.4106772;
   return axios({
     method: 'get',
-    url: `${url}&type=${place}&location=${lat},${long}&key=${key}`
-  })
+    url: `${radar}location=${userLat,userLong}&radius=4827&type=${typeOfPlace}&key=${key}`
+  .then(response => response.data)
+  .catch(error => console.error(error));
+};
+
+const getDistanceData = (arrayofPlaces, userLat,userLong) => {
+  userLat = userLat || 37.7825177;
+  userLong = userLong || -122.4106772;
+  //place_id:ChIJ53I1Yn2AhYAR_Vl1vNygfMg|
+  destinationString = 
+  return axios({
+    method: 'get',
+    url: `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${userLat,userlong}&destinations=${destinationString}&key=${key}mode=transit&departure_time=now&transit_mode=bus|rail`
   .then(response => response.data)
   .catch(error => console.error(error));
 };
