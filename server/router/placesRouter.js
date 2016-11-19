d7a984c85211966c58656f67ed29cdfed02cb2f8
William Simmons <willsimmons@gmail.com>
2016-11-18 20:00:11 -0800

    interesting fact, google places search gives us hotels for parks

--- server/router/placesRouter.js
+++ server/router/placesRouter.js
@@ -3,8 +3,25 @@ const express = require('express');
 const placesController = require('../controller/placesController');
 const placesRouter = express.Router(); 
 
-// const testArray = ["ChIJCdSUgO-AhYARuk0zTH3lyvU","ChIJ82ZgT_GAhYAR4rJh5-mnw9I","ChIJnbTAq--AhYAReI41AUmRd1w","ChIJtfiHddh_j4ARAjjq3GVQZdI"];
 
+placesRouter.get('/museum', (req, res) =>{
+  let bundle = [];                                      //req.body.lat req.body.long
+  placesController.getData('museum', 37.7825177, -122.4106772)
+  .then(data => {
+    data.results.forEach((place) => bundle.push(place.place_id));  
+    placesController.getDistanceData(bundle.splice(0, 24), 37.7825177, -122.4106772)
+    .then(data => {
+      console.log(data);
+      res.status(200).json(data);
+    })
+    .catch(err => res.sendStatus(500));
+  })
+  .catch(err => res.sendStatus(500));
+});
+
+
+//DEV Testing 
+// const testArray = ["ChIJCdSUgO-AhYARuk0zTH3lyvU","ChIJ82ZgT_GAhYAR4rJh5-mnw9I","ChIJnbTAq--AhYAReI41AUmRd1w","ChIJtfiHddh_j4ARAjjq3GVQZdI"];
 //this places nearby search
 placesRouter.get('/devTest1', (req, res) =>{
   // console.log('look for coordinates in here', req);
