59bbea359b00ea1288c3afdb0000c539ff31869b
Daniel Shneyder <archcorsair@gmail.com>
2016-11-20 14:13:20 -0800

    Build name update, new icon

--- traveller/App/Themes/Images.js
+++ traveller/App/Themes/Images.js
@@ -3,19 +3,12 @@
 // leave off @2x/@3x
 const images = {
   bg: require('../Images/BG.png'),
+  logo: require('../Images/Logo.png'),
   igniteLogo1: require('../Images/ignite_logo1.png'),
   ir: require('../Images/ir.png'),
   plane: require('../Images/plane.png'),
   tileBg: require('../Images/tile_bg.png'),
   topLogo: require('../Images/top_logo.png'),
-  logo: require('../Images/ir.png'),
-  // clearLogo: require('../Images/top_logo.png'),
-  clearLogo: require('../Images/plane.png'),
-  // ignite: require('../Images/ignite_logo.png'),
-  ignite: require('../Images/plane.png'),
-  tileBg: require('../Images/tile_bg.png'),
-  background: require('../Images/BG.png'),
-  plane: require('../Images/plane.png'),
 }
 
 export default images
