4e284bfd4bfdca60a90378ebde51e00427a5b562
William Simmons <willsimmons@gmail.com>
2016-11-21 16:47:40 -0800

    cleanup and removal of 'dev' routes

--- server/router/placesRouter.js
+++ server/router/placesRouter.js
@@ -24,41 +24,6 @@ placesRouter.get('/health', (req, res) => {
   placesController.getGoogleData(req, res, 'bank');
 });
 
-//DEV Testing 
-// const testArray = ["ChIJCdSUgO-AhYARuk0zTH3lyvU","ChIJ82ZgT_GAhYAR4rJh5-mnw9I","ChIJnbTAq--AhYAReI41AUmRd1w","ChIJtfiHddh_j4ARAjjq3GVQZdI"];
-//this places nearby search
-placesRouter.get('/devTest1', (req, res) =>{
-  //log('look for coordinates in here', req);
-  placesController.getData('museum', 37.7825177, -122.4106772)
-  .then(data => {
-    //log(data);
-    res.status(200).json(data);
-  })
-  .catch(err => res.sendStatus(500));
-});
-
-//places in a radar radius
-placesRouter.get('/devTest2', (req, res) =>{
-  //log('look for coordinates in here', req);
-  placesController.getRadarData('museum', 37.7825177, -122.4106772)
-  .then(data => {
-    //log(data);
-    res.status(200).json(data);
-  })
-  .catch(err => res.sendStatus(500));
-});
-
-//how to get to an array of places, maximum of 25 in array
-placesRouter.get('/devTest3', (req, res) =>{
-  //log('look for coordinates in here', req);
-  placesController.getDistanceData(testArray, 37.7825177, -122.4106772)
-  .then(data => {
-    //log(data);
-    res.status(200).json(data);
-  })
-  .catch(err => res.sendStatus(500));
-});
-
 //404 all other routes
 placesRouter.use('*', (req, res) => res.status(404).send());
 
