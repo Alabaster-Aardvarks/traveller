f3db8067c16a8428cdd737ab47527b38c5b66807
Tim Arioto <timarioto@gmail.com>
2016-11-23 17:16:19 -0800

    change name of background image

--- traveller/App/Containers/SettingsScreen.js
+++ traveller/App/Containers/SettingsScreen.js
@@ -26,7 +26,7 @@ class SettingsScreen extends React.Component {
 
     return (
       <View style={styles.mainContainer}>
-        <Image source={Images.bg} style={styles.backgroundImage} resizeMode='stretch' />
+        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
         <ScrollView style={styles.container}>
 
           <View style={{flex:1}}>
