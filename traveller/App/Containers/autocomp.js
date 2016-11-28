6f096a019d9d0d184442e243ba0c7eb145d5b402
William Simmons <willsimmons@gmail.com>
2016-11-28 11:29:58 -0800

    adjustmentes to autocomp

--- traveller/App/Containers/autocomp.js
+++ traveller/App/Containers/autocomp.js
@@ -8,11 +8,14 @@ const token = process.env.GOOGLE_KEY || Secrets.GOOGLE_KEY // google API key
 const serverUrl = process.env.AUTOCOMP_SERVER_URL || Secrets.AUTOCOMP_SERVER_URL
 const api = create({ baseURL: serverUrl })
 
-export const getAutoComp = (partial) => {
+const formatter = (text) 
 
+
+export const getAutoComp = (partial) => {
+  let search = formatter(partial);
   if (debug) console.tron.display({ name: 'potential values', value: 'fetching ' + partial })
 
-  return api.get(``, { lat: position.latitude, long: position.longitude })
+  return api.get(`https://maps.googleapis.com/maps/api/place/queryautocomplete/json?key=${token}&input=${search}`, { lat: position.latitude, long: position.longitude })
   .then(resp => {
     if (!resp.ok) {
       console.tron.error(`Could not fetch ${partial} autocomplete from server [${resp.problem}]`)
