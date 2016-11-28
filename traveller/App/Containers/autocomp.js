3b6be03b005809bbcfa131d664e5fa1e9c6bda47
William Simmons <willsimmons@gmail.com>
2016-11-28 11:29:10 -0800

    first autocomplete attempt

---
+++ traveller/App/Containers/autocomp.js
@@ -0,0 +1,28 @@
+import { create } from 'apisauce'
+import { encode } from 'base-64'
+import Secrets from 'react-native-config'
+
+const debug = false
+
+const token = process.env.GOOGLE_KEY || Secrets.GOOGLE_KEY // google API key
+const serverUrl = process.env.AUTOCOMP_SERVER_URL || Secrets.AUTOCOMP_SERVER_URL
+const api = create({ baseURL: serverUrl })
+
+export const getAutoComp = (partial) => {
+
+  if (debug) console.tron.display({ name: 'potential values', value: 'fetching ' + partial })
+
+  return api.get(``, { lat: position.latitude, long: position.longitude })
+  .then(resp => {
+    if (!resp.ok) {
+      console.tron.error(`Could not fetch ${partial} autocomplete from server [${resp.problem}]`)
+      return
+    }
+
+    if (debug) console.tron.display({ name: 'getPlaces response', value: resp.data })
+
+    return resp.data
+    
+  })
+
+}
