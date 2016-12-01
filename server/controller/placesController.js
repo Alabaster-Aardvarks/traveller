d6e52530b7ad8c3606f22de84a9a21f460dd68fd
William Simmons <willsimmons@gmail.com>
2016-11-30 21:07:28 -0800

    promisified and divided all places requests, need to figure out rate limit

--- server/controller/placesController.js
+++ server/controller/placesController.js
@@ -91,14 +91,14 @@ const getDetailDataTwo = (placeID) =>{
 };
 ////end of experiemental code
 //google distance matrix 
-const getDistanceData = (arrayOfPlaces, lat, long, mode) => {
+const getDistanceData = (arrayOfPlaces, lat, long, mode, start, end) => {
   
   lat = lat || 37.7825177;
   long = long || -122.4106772;
   mode = modeKeys[mode] || 'transit';
   let destinationString = 'place_id:';
   for (let i = 0; i < arrayOfPlaces.length; i++) {
-    destinationString += arrayOfPlaces[i];
+    destinationString += arrayOfPlaces[i].id;
     if (i !== arrayOfPlaces.length - 1 ) {
       destinationString += '|place_id:';
     }
@@ -111,6 +111,8 @@ const getDistanceData = (arrayOfPlaces, lat, long, mode) => {
   })
   .then(response => {
     log('getDistanceData response', response.data);
+    response.data.start = start;
+    response.data.end = end;
     return response.data;
   })
   .catch(error => console.error(error));
@@ -121,66 +123,52 @@ const getGoogleData = (req, res, keyword) => {
   long = req.query.long || -122.4106772;
   radius = req.query.radius || 50000;
   mode = req.query.mode || 'transit';
-  date = req.query.date || 'now'; 
-  //take results of nearby search and get their place ids
-  let idList = [];
-  let coordinates = [];     
-  //holder for response object
+  date = req.query.date || 'now';
+  size = req.query.size || 200;   
   let result = [];
-  //to iterate over results
-  let counter = 0;      
+  let counter = 0;
+  //grab radar ids     
   getRadarData(keyword, lat, long, radius)
   .then(data => {
+    let radarResults = [];
     if (!data.results.length) {
       console.error(`No google places data found for ${keyword}`);
       res.sendStatus(500).send({error: 'no data found by Google'});
       return;
     }
-    log(data.results.length);
-    log(data);
     data.results.forEach(place => {
-      idList.push(place.place_id);
-      coordinates.push(place.geometry.location);
-    });
-    //can go up to 100 per call for distance, but then we'll hit rate limits 
-    //currently i'm under the impression that we can do 200 quickly, which leads to 
-    //50 places per "button"/isochrone change
-    let shortList = idList.splice(0, 24); 
-    log(shortList);
-    getDistanceData(shortList, lat, long, mode)
-    .then(data => {
-      if (data.error_message) {
-        //this will trigger if we hit the rate limit TIME or DAILY limit
-        console.error(`Not able to get distance data for ${keyword} [${data.error_message}]`);
-        //to do: in case of api limits, we need to find another api to list the place names or poi's
-      } else {
-        data.destination_addresses.forEach(place => {
-          place = place.split('').splice(0, place.indexOf(',')).join('');
-          result.push({
-            'name': place, 
-            'time': data.rows[0].elements[counter].duration.text,
-            'location': coordinates[counter],
-            'distance': data.rows[0].elements[counter].distance.text,
-            'metric distance': data.rows[0].elements[counter].distance.value 
-          });
-          counter++;
-        });
-        // console.log(result);
-        //there is a potential of an empty object being sent in case of breaking the rate limit!
-        res.status(200).json(result);
+      radarResults.push({
+        id: place.place_id,
+        coordinates: place.geometry.location
+      });
+    }); 
+    radarResults = radarResults.splice(0, size);
+    return radarResults;
+  })
+  .then(results => {
+    Promise.all(results).then(values => {
+      let currentPosition = 0;
+      let currentEnd = 24;
+      while (currentPosition < values.length) {
+        console.log('looping, currenty on', currentPosition, currentEnd);
+        let test = values.slice(currentPosition, currentEnd);
+         getDistanceData(test, lat, long, mode, currentPosition, currentEnd)
+        .then(data => {
+          console.log(`parse this###${data.start}-${data.end}###########`, data);
+        })
+        .catch(error => console.error(error));
+        currentPosition = currentEnd;
+        currentEnd = (currentEnd + 25 > values.length) ? values.length : currentEnd + 25;
       }
     })
-    .catch(err => {
-      console.error(`Not able to get distance data for ${keyword} [${err}]`);
-      res.sendStatus(500).send({error: `no distance data found for ${keyword}`});
-    });
+    .then(() => res.sendStatus(200).json(result))
+    .catch(err => console.error(error));
   })
-  .catch(err => {
-    console.error(err);
-    res.sendStatus(500).send({error: 'catch error, no response from API, check server code'});
-  });
+  .catch(err => console.error(error));
 };
 
 module.exports = {
   getData, getRadarData, getDistanceData, getGoogleData, getDetailData
 };
+    
+   
