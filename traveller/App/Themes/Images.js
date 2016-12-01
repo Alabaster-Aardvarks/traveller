2d441a60332dfd84af357d219838633d3661bcca
Tim Arioto <timarioto@gmail.com>
2016-12-01 11:30:10 -0800

    add new images for markers

--- traveller/App/Themes/Images.js
+++ traveller/App/Themes/Images.js
@@ -2,15 +2,12 @@
 
 // leave off @2x/@3x
 const images = {
-  background: require('../Images/BG.png'),
+  bg: require('../Images/BG.png'),
   logo: require('../Images/Logo.png'),
-  igniteLogo1: require('../Images/ignite_logo1.png'),
-  ir: require('../Images/ir.png'),
-  plane: require('../Images/plane.png'),
+  checkmark: require('../Images/checkmark.png'),
+  park1: require('../Images/park1.png'),
+  park2: require('../Images/park2.png'),
   tileBg: require('../Images/tile_bg.png'),
-  topLogo: require('../Images/top_logo.png'),
-  launchScreen55: require('../Images/Launch55.png'),
-  launchScreen47: require('../Images/Launch47.png'),
 }
 
 export default images
