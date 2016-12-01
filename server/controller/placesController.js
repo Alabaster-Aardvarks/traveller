8ebd621a2a45768e1180f9e1101429475909bb82
William Simmons <willsimmons@gmail.com>
2016-12-01 15:34:07 -0800

    places now reply with all potential places, up to 200 (google limit)

--- server/controller/placesController.js
+++ server/controller/placesController.js
@@ -2,6 +2,10 @@
 //api guide https://developers.google.com/maps/documentation/javascript/places#place_search_requests
 //keyword guide https://developers.google.com/places/supported_types
 const axios = require('axios');
+const debounce = require('./debounce.js');
+const axiosRetry = require('axios-retry');
+axiosRetry(axios, { retries: 3 });
+
 //1609 meters = 1 mile
 //4827 meters = 3 miles
 
@@ -18,7 +22,7 @@ const distance = 'https://maps.googleapis.com/maps/api/distancematrix/json?';
 
 const key = process.env.GOOGLE_KEY || GOOGLE_KEY;
 
-//how to use google places nearby
+//how to use google places nearby search *unused*
 const getData = (place, lat, long) => {
   lat = lat || 37.7825177;
   long = long || -122.4106772;
@@ -30,7 +34,7 @@ const getData = (place, lat, long) => {
   .catch(error => console.error(error));
 };
 
-//how to use google radar search -searching in a 50 mile area
+//how to use google radar search
 const getRadarData = (place, lat, long, radius) => {
   lat = lat || 37.7825177;
   long = long || -122.4106772;
@@ -39,14 +43,11 @@ const getRadarData = (place, lat, long, radius) => {
     method: 'get',
     url: `${radar}location=${lat},${long}&radius=${radius}&type=${place}&key=${key}` // FIXME: distance needs to be sent by client
   })
-  .then(response => {
-    //log('getRadarData response', response.data);
-    return response.data;
-  })
+  .then(response => response.data)
   .catch(error => console.error(error));
 };
 
-//converting client mode of transportation query to google api mode terms
+//converting client mode of transportation query to match google api requirements
 const modeKeys = {
   car: 'driving',
   bike: 'cycling',
@@ -54,8 +55,8 @@ const modeKeys = {
   transit: 'transit'
 };
 
+//google place detail search *unused*
 const getDetailData = (req, res, placeID) =>{
-  let results = [];
   let placeInfo = {};
   return axios({
     method: 'get',
@@ -66,33 +67,16 @@ const getDetailData = (req, res, placeID) =>{
       name: response.data.result.name,
       url: response.data.result.url,
     };
-    log('getDistanceData response', response.data);
     return res.status(200).json(placeInfo);
   })
   .catch(error => console.error(error));
 };
-////experimental code here
-const getDetailDataTwo = (placeID) =>{
-  let results = [];
-  let placeInfo = {};
-  return axios({
-    method: 'get',
-    url: `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeID}&key=${key}`
-  })
-  .then(response => {
-    placeInfo = {
-      name: response.data.result.name,
-      url: response.data.result.url,
-    };
-    log('getDistanceData response', response.data);
-    return placeInfo;
-  })
-  .catch(error => console.error(error));
-};
-////end of experiemental code
-//google distance matrix 
-const getDistanceData = (arrayOfPlaces, lat, long, mode, start, end) => {
-  
+
+// Google distance matrix throttler to avoid rate limiting
+const throttledDistanceAPI = debounce.promiseDebounce(axios, 1000, 4);
+
+//google distance matrix api
+const getDistanceData = (arrayOfPlaces, lat, long, mode) => {
   lat = lat || 37.7825177;
   long = long || -122.4106772;
   mode = modeKeys[mode] || 'transit';
@@ -103,21 +87,15 @@ const getDistanceData = (arrayOfPlaces, lat, long, mode, start, end) => {
       destinationString += '|place_id:';
     }
   }
-  log('getDistanceData destinationString', destinationString);
-
-  return axios({
+  return throttledDistanceAPI({
     method: 'get',
     url: `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${lat},${long}&destinations=${destinationString}&key=${key}&mode=${mode}&departure_time=now`
   })
-  .then(response => {
-    log('getDistanceData response', response.data);
-    response.data.start = start;
-    response.data.end = end;
-    return response.data;
-  })
+  .then(response => response.data)
   .catch(error => console.error(error));
 };
 
+//google api handler, chains radar and distance matrix results
 const getGoogleData = (req, res, keyword) => {
   lat = req.query.lat || 37.7825177;
   long = req.query.long || -122.4106772;
@@ -125,15 +103,12 @@ const getGoogleData = (req, res, keyword) => {
   mode = req.query.mode || 'transit';
   date = req.query.date || 'now';
   size = req.query.size || 200;   
-  let result = [];
-  let counter = 0;
-  //grab radar ids     
+  //grab ids from radar search    
   getRadarData(keyword, lat, long, radius)
   .then(data => {
     let radarResults = [];
     if (!data.results.length) {
       console.error(`No google places data found for ${keyword}`);
-      res.sendStatus(500).send({error: 'no data found by Google'});
       return;
     }
     data.results.forEach(place => {
@@ -144,27 +119,24 @@ const getGoogleData = (req, res, keyword) => {
     }); 
     radarResults = radarResults.splice(0, size);
     return radarResults;
+  }) //take radar results and throttle them into distance matrix api
+  .then(results => {  
+    const divider = 25;
+    return Promise.all(
+      //break up the radar array into groups of 25, then call api
+      [...Array(Math.ceil(results.length / divider))].map((v, index) => {
+        const values = results.slice(index * divider, (index + 1) * divider);
+        return getDistanceData(values, lat, long, mode)
+        .then(data => data);
+      })
+    )
+    .then(dataArray => {
+      dataArray.forEach(result => console.log(result.destination_addresses.length));
+      let googleStuff = [].concat(...dataArray);
+      res.status(200).json(dataArray);
+    });
   })
-  .then(results => {
-    Promise.all(results).then(values => {
-      let currentPosition = 0;
-      let currentEnd = 24;
-      while (currentPosition < values.length) {
-        console.log('looping, currenty on', currentPosition, currentEnd);
-        let test = values.slice(currentPosition, currentEnd);
-         getDistanceData(test, lat, long, mode, currentPosition, currentEnd)
-        .then(data => {
-          console.log(`parse this###${data.start}-${data.end}###########`, data);
-        })
-        .catch(error => console.error(error));
-        currentPosition = currentEnd;
-        currentEnd = (currentEnd + 25 > values.length) ? values.length : currentEnd + 25;
-      }
-    })
-    .then(() => res.sendStatus(200).json(result))
-    .catch(err => console.error(error));
-  })
-  .catch(err => console.error(error));
+  .catch(error =>console.error(error));
 };
 
 module.exports = {
